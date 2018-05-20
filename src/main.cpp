b9719e3587c8b85c34e9b0d9ba38658313510d6c
Yunpeng Hou <yunpenghou@Yunpengs-MacBook-Pro.local>
2018-05-20 10:09:23 -0400

    init commit add cmake list

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+// main created by yhou46 2018-05-20
+
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <deque>
+#include <list>
+#include <queue>
+#include <sstream>
+#include <algorithm>
+#include <stack>
+
+#include <cctype>
+
+#include <set>
+#include <utility>
+#include <map>
+#include <memory>
+#include <regex>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+	
+	std::cout << "test\n";
+    
+    return 0;
+}
