7777ad9731d38d4fc0d6652725fa224f243d8b8c
Yunpeng Hou <yunpenghou-macbookpro2023@Yunpengs-MacBook-Pro.local>
2024-01-18 21:36:36 -0800

    add VsCode Cmake settings

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -24,17 +24,46 @@
 #include <ctime>
 
 //Self
-#include "LC_Solution20.h"
+//#include "LC_Solution20.h"
 
 using namespace std;
+
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int size = matrix.size();
+        // vector<vector<int> > result;
+        // result.resize(size, std::vector<int>(size, 0));
+        // result[0] = matrix[0];
+        for(int i=1; i < size; ++i) {
+            for(int j=0; j < size; ++j) {
+                if (j == 0) {
+                    matrix[i][j] = std::min(matrix[i-1][j], matrix[i-1][j+1]) + matrix[i][j];
+                }
+                else if (j == size-1) {
+                    matrix[i][j] = std::min(matrix[i-1][j-1], matrix[i-1][j]) + matrix[i][j];
+                }
+                else {
+                    matrix[i][j] = std::min(matrix[i-1][j-1], std::min(matrix[i-1][j], matrix[i-1][j+1])) + matrix[i][j];
+                }
+                std::cout << matrix[i][j] << "\n";
+            }
+        }
+        int minVal = matrix[size-1][0];
+        for(int i=0; i < size; ++i) {
+            minVal = std::min(minVal, matrix[size-1][i]);
+        }
+        return minVal;
+    }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
 	
     Solution s;
-    string input = "{{{";
+    vector<vector<int>> input = {{2,1,3}, {6,5,4}, {7,8,9}};
     
-    std::cout << s.isValid(input) << "\n";
+    std::cout << s.minFallingPathSum(input) << "\n";
     
     return 0;
 }
