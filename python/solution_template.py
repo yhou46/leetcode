b4ef6db3c30d8b4ffade1f587f18dfe34dab0ab4
Yunpeng Hou <yunpenghou-macbookpro2023@Yunpengs-MacBook-Pro.local>
2025-05-23 16:15:38 -0700

    minor fix to template

--- python/solution_template.py
+++ python/solution_template.py
@@ -2,9 +2,10 @@ import random
 from collections import deque, OrderedDict
 from typing import List
 
+# Description: TBD
 class Solution:
     pass
 
 if __name__ == "__main__":
-    pass
     # Run the solution code here
+    pass
